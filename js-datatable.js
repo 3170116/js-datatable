2cb9fa170f5c228f4b2186ecca1a4722d88f6f10
dimitris_bek <p3170116@gmail.com>
2022-01-06 18:03:55 +0200

    Added "client" type.

--- js-datatable.js
+++ js-datatable.js
@@ -1,31 +1,43 @@
 function newJsDataTable(id, options) {
 
+    //Define an array which will contain the data
+    let rows = [];
+
     //Find tag 'div' with id = 'id'
     let dt = document.getElementById(id);
 
     //Add a 'table' tag
-    dt.innerText = '<table class="js-table table table-striped table-bordered">';
+    let html = '<table class="js-table table table-striped table-bordered">';
 
     //Add headers
-    if (options.type == 'demo') {
-        let headers = '<tr class="js-table-header"><th scope="col">First Name</th><th scope="col">Job</th><th class="js-table-number" scope="col">Age</th></tr>'
-        dt.innerText += headers;
+    if (options.columns) {
+        let headers = '<tr class="js-table-header">'
+        
+        for (let i = 0; i < options.columns.length; i++) {
+            headers += '<th scope="col">' + options.columns[i].header + '</th>'
+        }
+
+        headers += '</tr>'
+
+        html += headers;
     }
 
     //Add rows
-    if (options.type == 'demo') {
-        let row1 = '<tr class="js-table-row"><td>John</td><td>Math Teacher</td><td class="js-table-number">27</td></tr>';
-        dt.innerText += row1;
+    if (options.type == 'client') {
+        for (let i = 0; i < options.data.length; i++) {
+            let row = '<tr class="js-table-row">';
 
-        let row2 = '<tr class="js-table-row"><td>Alex</td><td>Softwear Engineer</td><td class="js-table-number">34</td></tr>';
-        dt.innerText += row2;
+            for (let j = 0; j < options.data[i].length; j++) {
+                row += '<td class="js-table-cell">' + options.data[i][j] + '</td>';
+            }
 
-        let row3 = '<tr class="js-table-row"><td>Matina</td><td>Chef</td><td class="js-table-number">42</td></tr>';
-        dt.innerText += row3;
+            row += '</tr>';
+            html += row;
+        }
     }
 
     //End of 'table' tag
-    dt.innerText += '</table>'
-    dt.innerHTML = dt.innerText;
+    html += '</table>'
+    dt.innerHTML = html;
 
 }
